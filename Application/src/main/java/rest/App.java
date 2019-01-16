18877b2e028ad0b020efa144cee8c9d09aa51869
Admin <Admin@STUDENT19>
2019-01-16 10:28:14 +0000

    Small fixes to code to follow convention.

--- Application/src/main/java/rest/App.java
+++ Application/src/main/java/rest/App.java
@@ -7,50 +7,24 @@ import com.google.gson.Gson;
 import repositries.Account;
 
 public class App {
-	
+
 	private App() {
-		
+
 	}
 
 	public static void main(String[] args) {
 
-//		System.out.println("hello");
-//
-//		Gson gson = new Gson();
+		Gson gson = new Gson();
 
-//		Service s0 = new Service();
-//		Account a0 = new Account("Oussama", "Mlouk", 100000);
-
-//		s0.addToAccountMap(a0);
-//		String accountMap = gson.toJson(s0);
-//		String account0 = gson.toJson(a0);
-//
-//		System.out.println(accountMap);
-//		System.out.println(account0);
-		
-//		Account a0 = new Account("","",000000 );
-//		Account a1 = new Account("","",000000 );
-//		Service s0 = new Service();
-//		s0.addToAccountMap(a0);
-//		s0.addToAccountMap(a1);
-//		System.out.println(s0.retrieveAccountById(1));
-//		
-////		s0.searchAccount("");
-//		
-//		System.out.println(s0.retrieveAccountById(1).getFirstName().equals(""));
-//		System.out.println(s0.searchAccount(""));
-		
-		Account a0 = new Account("","",000001);
-		Account a1 = new Account("","",000001);
-		Account a2 = new Account("","",000001);
-		Account a3 = new Account("","",000001);
 		Service s0 = new Service();
-		s0.addToAccountMap(a0);
-		s0.addToAccountMap(a1);
-		s0.addToAccountMap(a2);
-		s0.addToAccountMap(a3);
+		Account a0 = new Account("Oussama", "Mlouk", 100000);
 
-		System.out.println(s0.searchAccount(""));
+		s0.addToAccountMap(a0);
+		String accountMap = gson.toJson(s0);
+		String account0 = gson.toJson(a0);
 		
+		System.out.println(accountMap);
+		System.out.println(account0);
+
 	}
 }
